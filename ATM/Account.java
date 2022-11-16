a9d9c896ec88f3496cc4bbddf352aacf9249c315
Yingjie <52tiffany.yang@gmail.com>
2022-11-16 17:38:03 -0500

    add NumberGuess

--- ATM/Account.java
+++ ATM/Account.java
@@ -6,7 +6,7 @@ public class Account {
 	// variables
 	private int customerNumber;
 	private int pinNumber;
-	private double checkingBalance = 0;
+	private double checkingBalance;
 	private double savingBalance = 0;
 
 	Scanner input = new Scanner(System.in);
